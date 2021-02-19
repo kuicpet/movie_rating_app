26c1833f24619aec7a1b1a924e49461318547d38
Kingsley <kingsleyumujeyan@gmail.com>
2021-02-19 17:21:02 +0100

    ft--home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <router-link :to="{ name: 'Home' }" class="side_bar_link">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+          <v-list-tile-content>Home</v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+        <router-link :to="{ name: 'Contact' }" class="side_bar_link">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-tile-action>
+          <v-list-tile-content>Contact</v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="blue" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Home</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <div id="app">
+          <router-view/>
+        </div>
+      </v-container>
+    </v-content>
+    <v-footer color="blue" app>
+      <span class="white--text" >&copy; 2021</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -9,6 +46,12 @@ import "./assets/stylesheets/main.css";
 
 export default {
   name: 'App',
+  data: () => ({
+    drawer: null,
+  }),
+  props: {
+    source: String,
+  }
 };
 </script>
 
